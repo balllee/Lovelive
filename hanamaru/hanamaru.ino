419549968960a43a55a03872475b23af5db55748
balllee <balllee@users.noreply.github.com>
2017-02-07 10:06:48 +0800

    upload code

---
+++ hanamaru/hanamaru.ino
@@ -0,0 +1,20 @@
+#include <Keyboard.h>
+int key = 97;
+int num = 9;
+void setup() {
+  for(int i = 0;i < num;i++){
+    pinMode(i + 2,INPUT_PULLUP);
+  }
+  Keyboard.begin();
+}
+
+void loop() {
+  for(int i = 0;i < num;i++){
+    if(digitalRead(i+2) == LOW){
+      Keyboard.press(key + i);
+    }
+    if(digitalRead(i+2) == HIGH){
+      Keyboard.release(key + i);
+    }
+  }
+}
---
+++ hanamaru1/hanamaru1.ino
@@ -0,0 +1,20 @@
+#include <Keyboard.h>
+int key = 97;
+int num = 9;
+void setup() {
+  for(int i = 0;i < num;i++){
+    pinMode(i + 2,INPUT);
+  }
+  Keyboard.begin();
+}
+
+void loop() {
+  for(int i = 0;i < num;i++){
+    if(digitalRead(i+2) == HIGH){
+      Keyboard.press(key + i);
+    }
+    if(digitalRead(i+2) == LOW){
+      Keyboard.release(key + i);
+    }
+  }
+}
